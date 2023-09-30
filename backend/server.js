754faa12a565746b4734d41f13e44c1bc897d038
BhanukaAGH <it20009472@my.sliit.lk>
2023-09-30 22:01:18 +0530

    proxy set

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ const applicantRouter = require('./routes/applicantRoutes')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.set('trust proxy', 1)
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000,
